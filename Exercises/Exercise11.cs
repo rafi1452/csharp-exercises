76533f7d5f498f94dd42dc6b63add322a8175899
Venu Thammisetty <venuthammisetty@resonous.com>
2022-11-04 09:56:10 +0530

    Deleted TextFile1.txt, committed by mistake

--- Exercises/Exercise11.cs
+++ Exercises/Exercise11.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
 using System.Data.SqlClient;
-using System.Data.SqlTypes;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-
 
 public class Exercise11
 {
@@ -25,37 +18,36 @@ public class Exercise11
             for (int i = 1; i < lines.Length; i++)
             {
                 string[] rowData = lines[i].Split(',');
-                if (!species_names.Contains(rowData[4]))
+                if (!species_names.Contains(rowData[rowData.Length - 1]))
                 {
-                    species_names.Add(rowData[4]);
+                    species_names.Add(rowData[rowData.Length - 1]);
                 }
                 SqlCommand cmd = new SqlCommand("insert into species.dbo.iris values(" + float.Parse(rowData[0])
                     + "," + float.Parse(rowData[1]) + "," + float.Parse(rowData[2]) + "," +
                     float.Parse(rowData[3]) + ",'" + rowData[4] + "');", conn);
                 cmd.ExecuteNonQuery();
             }
+
             foreach (string name in species_names)
             {
-                List<string> props = new List<string>() {"sl","sw","pl","pw" };
-                Console.WriteLine("\n" + name.ToUpper());
+                List<string> props = new List<string>() { "sl", "sw", "pl", "pw" };
+                Console.WriteLine("\n ----" + name.ToUpper() + "\n\tmin\tmax\tavg");
                 foreach (string prop in props)
                 {
-                    string retrieveQuery = $"SELECT MIN({prop}), MAX({prop}), AVG({prop}) from species.dbo.iris where species_name = '" + name + "';";
+                    string retrieveQuery = $"SELECT MIN({prop}), MAX({prop}), AVG({prop}) from species.dbo.iris where species_name = '{name}'";
                     SqlCommand cmd = new SqlCommand(retrieveQuery, conn);
                     using SqlDataReader rdr = cmd.ExecuteReader();
-                    Console.WriteLine("--->" + prop);
+                    Console.Write("\n" + prop);
                     while (rdr.Read())
                     {
-                        
-                        Console.WriteLine($" min = {rdr.GetDouble(0)}");
-                        Console.WriteLine($" max = {rdr.GetDouble(1)}");
-                        Console.WriteLine($" avg = {rdr.GetDouble(2).ToString("0.00")}");
+                        Console.Write($"\t{rdr.GetDouble(0).ToString("0.00")}");
+                        Console.Write($"\t{rdr.GetDouble(1).ToString("0.00")}");
+                        Console.WriteLine($"\t{rdr.GetDouble(2).ToString("0.00")}");
                     }
                 }
             }
             conn.Close();
         }
-        Console.ReadLine();
     }
 }
 
